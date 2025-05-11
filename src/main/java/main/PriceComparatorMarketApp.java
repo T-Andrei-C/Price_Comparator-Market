3bf8883e119aaa43b1790bc09246e8d02014d027
Andrew <toroaba_andrei@yahoo.com>
2025-05-11 17:29:37 +0300

    Updated PriceComparatorMarketApp class to be able to use the .env file

--- src/main/java/main/PriceComparatorMarketApp.java
+++ src/main/java/main/PriceComparatorMarketApp.java
@@ -2,8 +2,10 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("/.env")
 public class PriceComparatorMarketApp {
     public static void main(String[] args) {
         SpringApplication.run(PriceComparatorMarketApp.class, args);
