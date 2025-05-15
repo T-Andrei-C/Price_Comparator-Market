4f406f007d4b9a29ea419728b632dbf41962e680
Andrew <toroaba_andrei@yahoo.com>
2025-05-15 19:27:36 +0300

    Added a new method to SupermarketRepository for finding a supermarket using fields

--- src/main/java/main/repository/SupermarketRepository.java
+++ src/main/java/main/repository/SupermarketRepository.java
@@ -2,8 +2,31 @@ package main.repository;
 
 import main.model.Supermarket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SupermarketRepository extends JpaRepository<Supermarket, Long> {
+
+    @Query(
+            "SELECT s " +
+            "FROM Supermarket s " +
+            "WHERE s.supermarket_name = ?1 " +
+                    "AND s.product_name = ?2 " +
+                    "AND s.unit = ?3 " +
+                    "AND s.category = ?4 " +
+                    "AND s.quantity = ?5 " +
+                    "AND s.brand = ?6"
+    )
+    Supermarket findSupermarketByFields(
+            String supermarketName,
+            String productName,
+            String unit,
+            String category,
+            Double quantity,
+            String brand
+    );
+
+//    @Query("SELECT s FROM Supermarket s WHERE s.supermarket_name = ?1")
+//    Supermarket findSupermarketByName (String supermarketName);
 }
