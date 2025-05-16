54ef5dc287dfec085bb457b2e7cedcca6dec8858
Andrew <toroaba_andrei@yahoo.com>
2025-05-16 21:59:08 +0300

    Finished the business logic for showing best discounts for each product (needs refactoring, maybe clean)

---
+++ src/main/java/main/service/ProductService.java
@@ -0,0 +1,79 @@
+package main.service;
+
+import lombok.RequiredArgsConstructor;
+import main.helpers.DiscountCalculator;
+import main.model.DTO.ProductHighestDiscountDTO;
+import main.model.Discount;
+import main.model.Product;
+import main.model.Supermarket;
+import main.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Set<ProductHighestDiscountDTO> displayHighestDiscountForProducts() {
+        List<Product> products = productRepository.findAll();
+        LocalDate simulateDate = LocalDate.parse("2025-05-02");
+        Set<ProductHighestDiscountDTO> productsHighestDiscount = new HashSet<>();
+
+        for (Product product : products) {
+            List<Discount> highestDiscounts = new ArrayList<>();
+            for (Supermarket supermarket : product.getSupermarkets()) {
+                for (Discount discount : filterDiscountsByDate(supermarket.getDiscounts(), simulateDate)) {
+
+                    if (!highestDiscounts.isEmpty()) {
+                        if (discount.getPercentage_of_discount() > highestDiscounts.getFirst().getPercentage_of_discount()) {
+                            highestDiscounts.clear();
+                            highestDiscounts.add(discount);
+                        } else if (discount.getPercentage_of_discount().equals(highestDiscounts.getFirst().getPercentage_of_discount())) {
+                            highestDiscounts.add(discount);
+                        }
+                    } else {
+                        highestDiscounts.add(discount);
+                    }
+
+                }
+            }
+
+            for (Discount highestDiscount : highestDiscounts) {
+                productsHighestDiscount.add(
+                        ProductHighestDiscountDTO.builder()
+                                .product_name(product.getName())
+                                .supermarket_name(highestDiscount.getSupermarket().getName())
+                                .product_price(highestDiscount.getSupermarket().getProduct_price())
+                                .percentage_of_discount(highestDiscount.getPercentage_of_discount())
+                                .product_price_with_discount(DiscountCalculator.applyDiscountToProductPrice(highestDiscount.getSupermarket().getProduct_price(), highestDiscount.getPercentage_of_discount()))
+                                .brand(product.getBrand())
+                                .category(product.getCategory())
+                                .currency(highestDiscount.getSupermarket().getCurrency())
+                                .quantity(product.getQuantity())
+                                .build()
+                );
+            }
+        }
+
+        return productsHighestDiscount;
+    }
+
+    private Set<Discount> filterDiscountsByDate(Set<Discount> discounts, LocalDate simulateDate) {
+        return discounts.stream()
+                .filter(discount ->
+                        discount.getFrom_date().isEqual(simulateDate) ||
+                                discount.getTo_date().isEqual(simulateDate) ||
+                                simulateDate.isAfter(discount.getFrom_date()) ||
+                                simulateDate.isBefore(discount.getTo_date())
+                ).collect(Collectors.toSet());
+    }
+
+}
