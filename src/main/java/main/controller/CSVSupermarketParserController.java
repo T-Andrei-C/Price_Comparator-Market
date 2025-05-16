7741e962b6113e1447a8e88227073e66da5d1413
Andrew <toroaba_andrei@yahoo.com>
2025-05-16 15:12:26 +0300

    Updated CSVSupermarketParserController to use the new service called CSVSupermarketAndProductParserService

--- src/main/java/main/controller/CSVSupermarketParserController.java
+++ src/main/java/main/controller/CSVSupermarketParserController.java
@@ -1,7 +1,8 @@
 package main.controller;
 
 import lombok.RequiredArgsConstructor;
-import main.service.CSVSupermarketParserService;
+//import main.service.CSVSupermarketParserService;
+import main.service.CSVSupermarketAndProductParserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,10 +14,10 @@ import java.io.IOException;
 @RequestMapping("/price_comparator/api/v1/supermarket/csv")
 public class CSVSupermarketParserController {
 
-    private final CSVSupermarketParserService CSVSupermarketParserService;
+    private final CSVSupermarketAndProductParserService CSVSupermarketAndProductParserService;
 
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ResponseEntity<String> uploadCSVFiles (@RequestParam("file") MultipartFile file) throws IOException {
-        return ResponseEntity.ok(CSVSupermarketParserService.uploadCSVFile(file));
+        return ResponseEntity.ok(CSVSupermarketAndProductParserService.uploadCSVFile(file));
     }
 }
