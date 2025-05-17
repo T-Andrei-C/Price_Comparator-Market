a93f4ed0ce87a2613824fdce6d6cfff6b115bb76
Andrew <toroaba_andrei@yahoo.com>
2025-05-17 23:02:44 +0300

    moved simulateDate variable to a class to be used across all files

---
+++ src/main/java/main/helpers/SimulateDate.java
@@ -0,0 +1,11 @@
+package main.helpers;
+
+import java.time.LocalDate;
+
+public class SimulateDate {
+
+    public static LocalDate getDate () {
+        return LocalDate.parse("2025-05-01");
+    }
+
+}
