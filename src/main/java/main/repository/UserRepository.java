ebfa18a35848308f169d2ce5d4c1d42999e1e4ed
Andrew <toroaba_andrei@yahoo.com>
2025-05-22 22:04:18 +0300

    Created repository interfaces for each new model added: notification, targetproduct and user

---
+++ src/main/java/main/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package main.repository;
+
+import main.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail (String email);
+
+}
