9165c2fdedfd01f358eb9652f4268ba1b3f5bb18
Andrew <toroaba_andrei@yahoo.com>
2025-05-23 15:22:14 +0300

    Added a new method to DiscountCalculator for verifing if discount can be applied

--- src/main/java/main/helpers/DiscountCalculator.java
+++ src/main/java/main/helpers/DiscountCalculator.java
@@ -1,9 +1,18 @@
 package main.helpers;
 
+import main.model.Discount;
+
+import java.time.LocalDate;
+
 public class DiscountCalculator {
 
     public static Double applyDiscountToProductPrice (Double price, Double percentageOfDiscount) {
         return price * (1 - (percentageOfDiscount / 100));
+    }
+
+    public static boolean checkIfDiscountApplies (LocalDate date, Discount discount) {
+        return (date.isEqual(discount.getFrom_date()) || date.isAfter(discount.getFrom_date())) &&
+                (date.isEqual(discount.getTo_date()) || date.isBefore(discount.getTo_date()));
     }
 
 }
