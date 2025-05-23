ecb821303997b9ac5a30194ce64b63193a08f5fd
Andrew <toroaba_andrei@yahoo.com>
2025-05-23 23:17:50 +0300

    Added comments to explain the logic behind the methods

--- src/main/java/main/helpers/CSVFileNameReader.java
+++ src/main/java/main/helpers/CSVFileNameReader.java
@@ -4,8 +4,9 @@ import java.time.LocalDate;
 import java.util.Arrays;
 
 public class CSVFileNameReader {
-    public static String getSupermarketName (String csvFileName) {
+    public static String getSupermarketName(String csvFileName) {
         String[] csvFileNameArray = csvFileNameArray(csvFileName);
+        //if the item can't be parsed by the LocalDate.parse, it means the item is the name of the supermarket
         for (String item : csvFileNameArray) {
             try {
                 LocalDate csvFilePublishDate = LocalDate.parse(item);
@@ -16,8 +17,10 @@ public class CSVFileNameReader {
         return "N/A";
     }
 
-    public static LocalDate getPublishDateName (String csvFileName) {
+
+    public static LocalDate getPublishDate(String csvFileName) {
         String[] csvFileNameArray = csvFileNameArray(csvFileName);
+        //if it finds a local date it will return it
         for (String item : csvFileNameArray) {
             try {
                 return LocalDate.parse(item);
@@ -28,7 +31,8 @@ public class CSVFileNameReader {
         return LocalDate.now();
     }
 
-    private static String[] csvFileNameArray (String csvFileName) {
+    //remove the .csv from the name and get the name of the file and date
+    private static String[] csvFileNameArray(String csvFileName) {
         return Arrays.stream(csvFileName.replaceAll(".csv", "").split("_"))
                 .filter(item -> !item.equalsIgnoreCase("discounts"))
                 .filter(item -> !item.equalsIgnoreCase("discount"))
