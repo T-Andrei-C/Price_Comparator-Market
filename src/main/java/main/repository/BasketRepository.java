e6e58ea8e16803ff9cb24bc1174b5f69c1fe2a2b
Andrew <toroaba_andrei@yahoo.com>
2025-05-23 15:18:47 +0300

    Created BasketRepository

---
+++ src/main/java/main/repository/BasketRepository.java
@@ -0,0 +1,9 @@
+package main.repository;
+
+import main.model.user.Basket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasketRepository extends JpaRepository<Basket, Long> {
+}
